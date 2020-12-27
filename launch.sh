c8c511832ac8a1198fd4f3b2031392fa694323cc
Han Na <han.na@tum.de>
2020-12-27 19:16:04 +0100

    Add default variables for easier use

--- launch.sh
+++ launch.sh
@@ -1,18 +1,58 @@
 #!/bin/bash
 
-unset flg OPTARG OPTIND
+app_dir_default="" # /path/to/your/source/code (on the host)
+data_dir_default="" # /path/to/your/data/folder (on the host)
+image_default="ga86caq/cudagl-optix:7.0-ubuntu14.04" # Image name
 
-while getopts ":a:f:" flg; do 
+unset flg OPTARG OPTIND
+shared_vol=""
+
+while getopts ":a:f:v:i:" flg; do 
     case $flg in
     	a) app_dir=$OPTARG;
-    	   printf "Load app source code from:\n$OPTARG\n";; # Mount app directory
+    	   printf "Load app source code from: $OPTARG\n";; # Mount app directory
     	f) data_dir=$OPTARG;
-    	   printf "Load data from:\n$OPTARG\n";; # Mount data directory
-    	\?) echo "Unknown flag name!" >&2;;
+    	   printf "Load data from: $OPTARG\n";; # Mount data directory
+		v) shared_vol="-v $OPTARG $shared_vol";; # Other shared volumn
+    	i) image=$OPTARG;
+    	   printf "Launch container from image $image\n";;
+    	\?) printf "Flag not understood...\nValid flags are:\n\t-a\t\"app\" source code directory on your host system (will mount to /app/ in countainer)\n\t-f\t\"file\" directory on you host system (will mount to /data/ in container)\n\t-v\tOther directory you want to mount, in format:\"-v /path/in/host/dir:/path/in/container\"\n\t-i\tImages to launch, default is the image \"ga86caq/cudagl-optix\"\n" >&2
+		   exit 1
+		;;
     esac
 done
 
-shared_vol="-v $app_dir:/app/ -v $data_dir:/data/"
+# Mount source code directory to /app
+if [ -z "$app_dir" ] 
+then 
+	if [ -n "$app_dir_default" ] 
+	then
+		shared_vol="-v $app_dir_default:/app/ $shared_vol"
+	fi
+else
+	shared_vol="-v $app_dir:/app/ $shared_vol"
+fi
+# Mount data directory to /data
+if [ -z "$data_dir" ] 
+then 
+	if [ -n "$data_dir_default" ] 
+	then
+		shared_vol="-v $data_dir_default:/data/ $shared_vol"
+	fi
+else
+	shared_vol="-v $data_dir:/data/ $shared_vol"
+fi
 
-docker run --gpus all --runtime=nvidia -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro $shared_vol --privileged \
-ga86caq/cudagl-optix:7.0-ubuntu14.04 /bin/bash
+if [ -z "$image" ]; then 
+	image=$image_default 
+fi
+
+docker run -it \
+           --gpus all \
+           --runtime=nvidia \
+           --privileged \
+           -e DISPLAY=$DISPLAY \
+           -e XAUTHORITY=$XAUTH \
+           -v /tmp/.X11-unix:/tmp/.X11-unix:ro $shared_vol \
+           -v $XAUTH:$XAUTH \
+           $image /bin/bash
